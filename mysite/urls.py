c28d60ff5c69d7876352d8bee96ac61bbb6e252d
Zoé <zoe.durteste-erca@laposte.net>
2017-02-11 16:48:23 +0100

    My Django Girls app, first commit

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+"""mysite URL Configuration
+
+[...]
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+]
